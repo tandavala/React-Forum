deb8761acde780a573123f9cf580d90494970eac
tandavala <jose.tandavala@gmail.com>
2019-12-17 20:49:45 +0100

    add the initial folder  struture

--- src/App.js
+++ src/App.js
@@ -4,21 +4,8 @@ import "./styles/index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "0 auto", textAlign: "center" }}>
+      <img style={{ height: "100vh" }} src={logo} alt="Logo" />
     </div>
   );
 }
