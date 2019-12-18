14b22b5240140c570dbaa38e506cf9432a511faf
tandavala <jose.tandavala@gmail.com>
2019-12-18 11:53:58 +0100

    configuring firebase and coding its helper class.
    the helper class contain the two method one for creating a new user
    and the other one to login the user

---
+++ src/Firebase/confg.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBzDvn91N1Cq8mPLl9m6JT0M438GsW7CY0",
+  authDomain: "angularapp-152f2.firebaseapp.com",
+  databaseURL: "https://angularapp-152f2.firebaseio.com",
+  projectId: "angularapp-152f2",
+  storageBucket: "angularapp-152f2.appspot.com",
+  messagingSenderId: "197011525682",
+  appId: "1:197011525682:web:24a3c34b7214408b136c2b",
+  measurementId: "G-B85WFM6J0V"
+};
+
+export default firebaseConfig;
