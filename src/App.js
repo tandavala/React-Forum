89d7f367317be0a894213b5b9bbc22e326ad62bd
tandavala <jose.tandavala@gmail.com>
2019-12-18 11:04:56 +0100

    add global style

--- src/App.js
+++ src/App.js
@@ -7,23 +7,27 @@ import ForgotPassword from "./Components/Auth/ForgotPassword";
 import SearchLink from "./Components/Link/SearchLinks";
 import LinkDetail from "./Components/Link/LinkDetail";
 import LinkList from "./Components/Link/LinkList";
+import Header from "./Header";
 
 function App() {
   return (
     <>
-      <h3>Ola mund</h3>
-
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" render={() => <Redirect to="/new/1" />} />
-          <Route path="/create" component={CreateLink} />
-          <Route path="/login" component={Login} />
-          <Route path="/forgot" component={ForgotPassword} />
-          <Route path="/search" component={SearchLink} />
-          <Route path="/top" component={LinkList} />
-          <Route path="/new/:page" component={LinkList} />
-          <Route path="/link/:linkId" component={LinkDetail} />
-        </Switch>
+        <div className="app-container">
+          <Header />
+          <div className="route-container">
+            <Switch>
+              <Route exact path="/" render={() => <Redirect to="/new/1" />} />
+              <Route path="/create" component={CreateLink} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot" component={ForgotPassword} />
+              <Route path="/search" component={SearchLink} />
+              <Route path="/top" component={LinkList} />
+              <Route path="/new/:page" component={LinkList} />
+              <Route path="/link/:linkId" component={LinkDetail} />
+            </Switch>
+          </div>
+        </div>
       </BrowserRouter>
     </>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { WithRouter, NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <img src="../logo.png" alt="Hooks News Logo" className="logo" />
+      <NavLink to="/" className="header-title">
+        Hooks news
+      </NavLink>
+    </div>
+  );
 };
 
 export default Header;
