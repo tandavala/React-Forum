6b2ca2db4bc8a5425f565cb390a647c5449f7044
tandavala <jose.tandavala@gmail.com>
2019-12-18 11:12:26 +0100

    adding the main header and navegation

--- src/Header.js
+++ src/Header.js
@@ -4,10 +4,32 @@ import { WithRouter, NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <div className="header">
-      <img src="../logo.png" alt="Hooks News Logo" className="logo" />
-      <NavLink to="/" className="header-title">
-        Hooks news
-      </NavLink>
+      <div className="flex">
+        <img src="/logo.png" alt="Hooks News Logo" className="logo" />
+        <NavLink to="/" className="header-title">
+          Hooks News
+        </NavLink>
+        <NavLink to="/" className="header-link">
+          new
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/top" className="header-link">
+          top
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/search" className="header-link">
+          search
+        </NavLink>
+        <div className="divider">|</div>
+        <NavLink to="/create" className="header-link">
+          submit
+        </NavLink>
+      </div>
+      <div className="flex">
+        <NavLink to="/login" className="header-link">
+          login
+        </NavLink>
+      </div>
     </div>
   );
 };
